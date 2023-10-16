52e5033cf93ab917027f9682ca2eb746454936e3
Calex Raffield <clxrffdman@gmail.com>
2023-10-15 23:53:34 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -21,3 +21,45 @@ if (ctx) {
 header.innerHTML = gameName;
 app.append(header);
 app.append(canvas);
+
+//drawing code from https://shoddy-paint.glitch.me/paint0.html
+
+const cursor = { active: false, x: 0, y: 0 };
+
+const paths = [];
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active && ctx) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  clearCanvas();
+});
+
+function clearCanvas() {
+  if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, 256, 256);
+  }
+}
