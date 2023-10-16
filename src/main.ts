4685ffdf10d1d9433f61100fdf0a3d60403875de
Calex Raffield <clxrffdman@gmail.com>
2023-10-15 20:49:01 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,17 @@ const gameName = "Calex's game";
 document.title = gameName;
 
 const header = document.createElement("h1");
+
+const canvas: HTMLCanvasElement = document.createElement("canvas");
+const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+canvas.width = 256;
+canvas.height = 256;
+
+if (ctx) {
+  ctx.fillStyle = "green";
+  ctx.fillRect(0, 0, 256, 256);
+}
+
 header.innerHTML = gameName;
 app.append(header);
+app.append(canvas);
